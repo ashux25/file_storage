befddb4171682438298453a84318392a9eba0b60
Ashutosh Nagrale <129092713+ashux25@users.noreply.github.com>
2024-04-23 11:05:19 +0000

    schema file added

--- convex/files.ts
+++ convex/files.ts
@@ -1,9 +1,13 @@
-import { mutation, query } from "./_generated/server";
-import { v } from "convex/values";
+import { action, mutation, query } from "./_generated/server";
+import { ConvexError, v } from "convex/values";
 
 export const createTask = mutation({
   args: { name: v.string() },
   handler: async (ctx, args) => {
+    const user = await ctx.auth.getUserIdentity();
+    if (!user) {
+      throw new ConvexError("You must be logged In");
+    }
     await ctx.db.insert("files", { name: args.name });
   },
 });

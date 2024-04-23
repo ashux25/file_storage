ccb36167caad017ccc23e0120dbddc945ef5f758
Ashutosh Nagrale <129092713+ashux25@users.noreply.github.com>
2024-04-23 07:42:22 +0000

    signout signin button

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,24 @@
 import { Button } from "@/components/ui/button";
-import { SignInButton } from "@clerk/nextjs";
+import {
+  SignInButton,
+  SignOutButton,
+  SignedIn,
+  SignedOut,
+} from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button>Hello</Button>
-      <SignInButton mode="modal" />
+      <SignedIn>
+        <SignOutButton>
+          <Button>Sign Out</Button>
+        </SignOutButton>
+      </SignedIn>
+      <SignedOut>
+        <SignInButton mode="modal">
+          <Button>Sign In</Button>
+        </SignInButton>
+      </SignedOut>
     </main>
   );
 }

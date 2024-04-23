db5b9b8d137b0117a4f50e9fe908a88e0f70fd7e
Ashutosh Nagrale <129092713+ashux25@users.noreply.github.com>
2024-04-23 07:33:23 +0000

    clerk working fine

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import ConvexClientProvider from "./ConvexClientProvider";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 

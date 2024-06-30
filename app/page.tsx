bd14f8d4513396ccccc1003b37ac07301c38ed87
Ashutosh Nagrale <129092713+ashux25@users.noreply.github.com>
2024-06-30 07:38:50 +0000

    form chnages

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,52 @@ import { Button } from "@/components/ui/button";
 import { useOrganization, useUser } from "@clerk/nextjs";
 import { useMutation, useQuery } from "convex/react";
 import { api } from "../convex/_generated/api";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const formSchema = z.object({
+  title: z.string().min(5).max(50),
+  file: z
+    .custom<FileList>((val) => val instanceof FileList, "required")
+    .refine((files) => files.length > 0, "required"),
+});
 
 export default function Home() {
   const organization = useOrganization();
   const user = useUser();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+      file: undefined,
+    },
+  });
+
+  const fileRef = form.register("file");
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
 
   let orgId: string | undefined = undefined;
   if (organization.isLoaded && user.isLoaded) {
@@ -20,19 +62,64 @@ export default function Home() {
     <main className="container mx-auto pt-12">
       <div className="flex justify-between items-center">
         <h1 className="text-4xl font-bold">Your Files</h1>
-        <Button
-          onClick={() => {
-            if (!orgId) {
-              return;
-            }
-            createFile({
-              name: "Hello Ashu",
-              orgId: orgId,
-            });
-          }}
-        >
-          Upload
-        </Button>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button
+              onClick={() => {
+                if (!orgId) {
+                  return;
+                }
+                createFile({
+                  name: "Hello Ashu",
+                  orgId: orgId,
+                });
+              }}
+            >
+              Upload
+            </Button>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle className="mb-8">Upload your File</DialogTitle>
+              <DialogDescription>
+                <Form {...form}>
+                  <form
+                    onSubmit={form.handleSubmit(onSubmit)}
+                    className="space-y-8"
+                  >
+                    <FormField
+                      control={form.control}
+                      name="title"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Title</FormLabel>
+                          <FormControl>
+                            <Input {...field} />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />{" "}
+                    <FormField
+                      control={form.control}
+                      name="file"
+                      render={() => (
+                        <FormItem>
+                          <FormLabel>File</FormLabel>
+                          <FormControl>
+                            <Input type="file" {...fileRef} />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                    <Button type="submit">Submit</Button>
+                  </form>
+                </Form>
+              </DialogDescription>
+            </DialogHeader>
+          </DialogContent>
+        </Dialog>
       </div>
       {files?.map((file) => {
         return <div key={file._id}>{file.name}</div>;
